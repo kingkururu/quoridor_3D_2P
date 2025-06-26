097cfe5e810e210fb6aa5ebbaf446ca472c13a7a
정채윤 <junghaejin79@gmail.com>
2025-06-26 13:50:11 +0900

    network-prototype

--- test/test-network/network.hpp
+++ test/test-network/network.hpp
@@ -4,7 +4,18 @@
 
 #if RUN_NETWORK 
 #include <iostream>
-
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+class NetworkManager {
+    public:
+    std::string getLocalIP();
+    void runHost(int port);
+    void runClient(const std::string& host_ip, int port);
+    void receiveData(int sock, char buffer[]);
+    void sendData(int sock, std::string& message);
+}
 #else 
 
 #endif 
